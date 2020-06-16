0a899bd02c86041a5f1debd52505626325985db8
Mustafiz04 <kaifee000@gmail.com>
2020-06-16 22:49:57 +0530

    Add few features

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke_container');
-const pokemons_number = 811;
+const pokemons_number = 1000;
 const colors = {
 	fire: '#FDDFDF',
 	grass: '#DEFDE0',
